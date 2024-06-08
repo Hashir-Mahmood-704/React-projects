f50b8e65f35809a85ae24c566ea206ccd326cebf
Hashir <hashir704x@gmail.com>
2024-06-08 07:05:49 -0700

    front page done

--- ecommerce-app/app_frontend/src/components/contact.tsx
+++ ecommerce-app/app_frontend/src/components/contact.tsx
@@ -8,20 +8,20 @@ import {
 
 const Contact = () => {
   return (
-    <div className="flex justify-center items-center bg-blue-600 py-[25px] px-[15px] text-white">
-      <div className="flex gap-[20px] lg:gap-0 flex-col lg:flex-row justify-center lg:justify-between items-center w-[60%]">
-        <span>BE IN TOUCH WITH US</span>
+    <div className="flex justify-center items-center bg-blue-600 py-[15px] lg:py-[25px] px-[15px] text-white">
+      <div className="flex gap-[14px] lg:gap-0 flex-col lg:flex-row  lg:justify-between items-center w-[60%]">
+        <span className="text-[14px] lg:text-[18px]">BE IN TOUCH WITH US</span>
         <div className="flex flex-col lg:flex-row items-center">
           <input
             placeholder="Enter here"
             type="text"
-            className="outline-none h-[40px] lg:border-r lg:rounded-l-lg text-black px-3 rounded-lg lg:rounded-none"
+            className="outline-none h-[30px] text-[14px] lg:text-[18px] lg:h-[40px] lg:border-r lg:rounded-l-lg text-black px-3 rounded-lg lg:rounded-none"
           />
-          <button className="bg-black h-[40px] px-3 rounded-lg lg:rounded-none mt-[14px] lg:mt-0 lg:rounded-r-lg">
+          <button className="bg-black h-[30px] text-[14px] lg:text-[18px]  lg:h-[40px] px-3 rounded-lg lg:rounded-none mt-[14px] lg:mt-0 lg:rounded-r-lg">
             Join Us
           </button>
         </div>
-        <div className="flex text-[20px] items-center gap-[8px]">
+        <div className="flex text-[16px] lg:text-[20px] items-center gap-[8px]">
           <FaFacebook />
           <FaInstagram />
           <FaTwitter />
